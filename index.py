c13c9312b0b11392be369cf443d0494cebc11cfc
rafaax <rafavfx1@gmail.com>
2023-08-01 08:53:52 -0300

    classes

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
-aaa
+from classes import *
+
+tv = Televisor('L41N', 'SERIAL EXPERIMENTS LAIN')
+controle = ControleRemoto(tv)
+
+controle.sintonizaCanal('S7')
+controle.trocaCanal('S7')
+
+print(tv.canal_atual)
+
+print(tv.lista_canais)
